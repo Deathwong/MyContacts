d731213c6984660897bc136d16d7f81458999b8e
MENSAH Mawulolo Jean-Baptiste <mmenah@et.esiea.fr>
2025-10-08 10:09:36 +0200

    ajout variable environment

--- client/src/services/api.js
+++ client/src/services/api.js
@@ -1,8 +1,9 @@
 // client/src/services/api.js
 import axios from "axios";
 
+const baseURL = process.env.REACT_APP_API_BASE_URL || "http://localhost:5050"; // default dev API
 const api = axios.create({
-    baseURL: "http://localhost:5050", // backend Express
+    baseURL,
 });
 
 api.interceptors.request.use((config) => {
